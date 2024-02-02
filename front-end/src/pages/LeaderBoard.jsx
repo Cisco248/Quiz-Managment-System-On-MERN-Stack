9c6f62becf7ff8a4e21fda8595032c743708a571
Lahiru Dilshan <129523905+Cisco248@users.noreply.github.com>
2024-02-02 22:38:46 +0530

    leader

---
+++ front-end/src/pages/LeaderBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navibar from '../component/Navibar'
+import Footer from '../component/Footer'
+
+function LeaderBoard() {
+  return (
+    <div>
+      <Navibar />
+      <Footer />
+    </div>
+  )
+}
+
+export default LeaderBoard
