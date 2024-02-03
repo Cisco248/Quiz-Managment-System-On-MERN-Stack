53890ec2e3c873e8766783d331444868758fcd3c
Lahiru Dilshan <129523905+Cisco248@users.noreply.github.com>
2024-02-03 21:57:02 +0530

    home

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -12,14 +12,13 @@ function Home() {
         </div>
         <div className={Style.home_page_container}>
             <div className={Style.tags}>
-                <div className={Style.box1}></div>
-                <div className={Style.box2}></div>
-                <div className={Style.box3}></div>
-                <div className={Style.box4}></div>
-                <div className={Style.box5}></div>
-                <div className={Style.box6}></div>
-                <div className={Style.box7}></div>
-                <div className={Style.box8}></div>
+                <button className={Style.box1}>Science</button>
+                <button className={Style.box2}>Maths</button>
+                <button className={Style.box3}>Biology</button>
+                <button className={Style.box4}>Technology</button>
+                <button className={Style.box5}>Music</button>
+                <button className={Style.box6}>Social Studies</button>
+                <button className={Style.box7}>Economics</button>
             </div>
             <div className={Style.leaderboar}>
                 <h3 className={Style.user_leaderboard}></h3>
@@ -29,12 +28,52 @@ function Home() {
                 <h3 className={Style.user_leaderboard}></h3>
             </div>
             <div className={Style.discover}>
-                <div className={Style.quiz_box1}></div>
-                <div className={Style.quiz_box2}></div>
-                <div className={Style.quiz_box3}></div>
-                <div className={Style.quiz_box4}></div>
-                <div className={Style.quiz_box5}></div>
-                <div className={Style.quiz_box6}></div>
+                <div className={Style.quiz_section_style}>
+                    <div className={Style.quiz_section_wrap}>
+                        <div className={Style.quiz_box}>
+                            <img className={Style.quiz_img} src="/maths.png" alt="" />
+                            <div className={Style.quiz_box_text}>
+                                <h4 className={Style.quiz_title}>Maths</h4>
+                                <h6 className={Style.quiz_creator}>Create by : Adithya</h6>
+                            </div>
+                        </div>
+                        <div className={Style.quiz_box}>
+                            <img className={Style.quiz_img} src="/biology.png" alt="" />
+                            <div className={Style.quiz_box_text}>
+                                <h4 className={Style.quiz_title}>Biology</h4>
+                                <h6 className={Style.quiz_creator}>Create by : Pasindu</h6>
+                            </div>
+                        </div>
+                        <div className={Style.quiz_box}>
+                            <img className={Style.quiz_img} src="/technology.png" alt="" />
+                            <div className={Style.quiz_box_text}>
+                                <h4 className={Style.quiz_title}>Malware</h4>
+                                <h6 className={Style.quiz_creator}>Create by : Ishita</h6>
+                            </div>
+                        </div>
+                        <div className={Style.quiz_box}>
+                            <img className={Style.quiz_img} src="/music.png" alt="" />
+                            <div className={Style.quiz_box_text}>
+                                <h4 className={Style.quiz_title}>Music</h4>
+                                <h6 className={Style.quiz_creator}>Create by : Lahiru</h6>
+                            </div>
+                        </div>
+                        <div className={Style.quiz_box}>
+                            <img className={Style.quiz_img} src="/science.png" alt="" />
+                            <div className={Style.quiz_box_text}>
+                                <h4 className={Style.quiz_title}>Animals</h4>
+                                <h6 className={Style.quiz_creator}>Create by : Lakshan</h6>
+                            </div>
+                        </div>
+                        <div className={Style.quiz_box}>
+                            <img className={Style.quiz_img} src="/economic.png" alt="" />
+                            <div className={Style.quiz_box_text}>
+                                <h4 className={Style.quiz_title}>Trade Center</h4>
+                                <h6 className={Style.quiz_creator}>Create by : xlk</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className={Style.users}>
                 <h3 className={Style.users_name}></h3>
