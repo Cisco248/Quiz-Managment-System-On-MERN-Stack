69ad2bef9d6c713e8a8303379f268213d1a137eb
Lahiru Dilshan <129523905+Cisco248@users.noreply.github.com>
2024-02-04 16:45:35 +0530

    slider

--- front-end/src/component/image slider/Slider.jsx
+++ front-end/src/component/image slider/Slider.jsx
@@ -1,33 +1,37 @@
-import { useState } from "react"
-import Style from './Slider.module.css'
-import Btnslider from './Btnslider'
-import Dataslider from "./Dataslider"
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import { slideImages} from './slideImages'
+import './Slider.css'
 
-function Slider() {
+const divStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '20px',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    borderRadius : '30px',
+    height: '400px'
+  }
 
-    const [slideIndex, setSlideIndex] = useState(1)
 
-    const nextSlide = () => {
-
-    }
-
-    const prevSlide = () => {
-
-    }
-
-  return (
-        <div className={Style.image_slider}>
-            {Dataslider.map((obj, index) => {
-                return (
-                    <div key={obj.id}>
-                        <img src={`/slider/img${index + 1}}.png`} />
-                    </div>
-                )
-            })}
-            <Btnslider moveSlide={nextSlide} direction={"next"} />
-            <Btnslider moveSlide={prevSlide} direction={"prev"} />
-        </div>
-  )
+const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Slide>
+         {slideImages.map((image, index)=> (
+            <div key={index} className='slider_layer'>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${image.url})` }}>
+                <span className='spanStyle'>{image.caption}</span>
+                <span className='descriptionStyle'>{image.description}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    )
 }
 
-export default Slider
+export default Slideshow
