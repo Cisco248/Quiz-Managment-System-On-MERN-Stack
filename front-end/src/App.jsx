35e9b7d7777771bc77b01bf372f73f8325057665
Lahiru <lahirud608@gmail.com>
2024-03-23 02:08:03 +0530

    modify

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,40 +1,40 @@
-import { Routes, Route } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import axios from 'axios'
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
 
-import Welcome from './pages/Welcome'
-import Register from './pages/Register'
-import Login from '../src/pages/Login'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import QuizCreatePage from './pages/QuizCreatePage'
-import Help from './pages/Help'
-import Maintaince from './pages/Maintaince'
-import Contact from './pages/Contact'
-import Leaderboard from './pages/LeaderBoard'
-import Library from './pages/Library'
+import Welcome from "./pages/Welcome";
+import Register from "./pages/Register";
+import Login from "../src/pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import QuizCreatePage from "./pages/QuizCreatePage";
+import Help from "./pages/Help";
+import Maintaince from "./pages/Maintaince";
+import Contact from "./pages/Contact";
+// import Leaderboard from './pages/LeaderBoard'
+import Library from "./pages/Library";
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = "http://localhost:8000";
 
 function App() {
   return (
     <>
-      <Toaster position='top-center' toastOptions={{duration: 3000}} />
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
       <Routes>
-        <Route path='/' element={<Welcome />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/quiz create' element={<QuizCreatePage />} />
-        <Route path='/help' element={<Help />} />
-        <Route path='/maintaince' element={<Maintaince />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/leaderboard' element={<Leaderboard />} />
-        <Route path='/library' element={<Library />} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/quiz create" element={<QuizCreatePage />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/maintaince" element={<Maintaince />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/leaderboard" element={<Leaderboard />} /> */}
+        <Route path="/library" element={<Library />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
--- front-end/src/pages/Leaderboard.jsx
+++ front-end/src/pages/Leaderboard.jsx
@@ -1,49 +1,48 @@
-import Navibar2 from '../component/Navibar2'
-import Footer from '../component/Footer'
-import Style from './Leaderboard.module.css'
+import Navibar2 from "../component/Navibar2";
+import Footer from "../component/Footer";
+import Style from "./Leaderboard.module.css";
 
 function Leaderboard() {
   return (
     <div>
       <Navibar2 />
       <div className={Style.leaderboard_page}>
-          <div className={Style.page_title}>
-              <h1 className={Style.title_name}>Leaderboard</h1>
-          </div>
-          <div className={Style.leaderboard_content}>
-            <div className={Style.main_box}>
-                <div className={Style.box1}>
-                        <h2 className={Style.user_number}>1</h2>
-                        <h2 className={Style.users_name}>Eshan Fernando</h2>
-                        <h3 className={Style.user_point}>1098 Points</h3>
-                </div>
-                <div className={Style.box2}>
-                        <h2 className={Style.user_number}>2</h2>
-                        <h2 className={Style.users_name}>Naveen Mendis</h2>
-                        <h3 className={Style.user_point}>1090 Points</h3>
-                </div>
-                <div className={Style.box3}>
-                        <h2 className={Style.user_number}>3</h2>
-                        <h2 className={Style.users_name}>Pathum Kavinda</h2>
-                        <h3 className={Style.user_point}>1085 Points</h3>
-                </div>
-                <div className={Style.box4}>
-                        <h2 className={Style.user_number}>4</h2>
-                        <h2 className={Style.users_name}>Shehan Perera</h2>
-                        <h3 className={Style.user_point}>1080 Points</h3>
-                </div>
-                <div className={Style.box5}>
-                        <h2 className={Style.user_number}>5</h2>
-                        <h2 className={Style.users_name}>Sahan Tharuka</h2>
-                        <h3 className={Style.user_point}>1060 Points</h3>
-                </div>
+        <div className={Style.page_title}>
+          <h1 className={Style.title_name}>Leaderboard</h1>
+        </div>
+        <div className={Style.leaderboard_content}>
+          <div className={Style.main_box}>
+            <div className={Style.box1}>
+              <h2 className={Style.user_number}>1</h2>
+              <h2 className={Style.users_name}>Eshan Fernando</h2>
+              <h3 className={Style.user_point}>1098 Points</h3>
             </div>
-              
+            <div className={Style.box2}>
+              <h2 className={Style.user_number}>2</h2>
+              <h2 className={Style.users_name}>Naveen Mendis</h2>
+              <h3 className={Style.user_point}>1090 Points</h3>
+            </div>
+            <div className={Style.box3}>
+              <h2 className={Style.user_number}>3</h2>
+              <h2 className={Style.users_name}>Pathum Kavinda</h2>
+              <h3 className={Style.user_point}>1085 Points</h3>
+            </div>
+            <div className={Style.box4}>
+              <h2 className={Style.user_number}>4</h2>
+              <h2 className={Style.users_name}>Shehan Perera</h2>
+              <h3 className={Style.user_point}>1080 Points</h3>
+            </div>
+            <div className={Style.box5}>
+              <h2 className={Style.user_number}>5</h2>
+              <h2 className={Style.users_name}>Sahan Tharuka</h2>
+              <h3 className={Style.user_point}>1060 Points</h3>
+            </div>
           </div>
+        </div>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Leaderboard
+export default Leaderboard;
