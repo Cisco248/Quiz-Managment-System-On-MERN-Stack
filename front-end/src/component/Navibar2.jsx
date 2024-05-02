e62836687e916e8611dd01591b9ec025a197de35
Lahiru <lahirud608@gmail.com>
2024-05-02 12:41:55 +0530

    final

--- front-end/src/component/Navibar2.jsx
+++ front-end/src/component/Navibar2.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Styles from './Navibar2.module.css';
 
 const Navibar2 = () => {
@@ -6,17 +6,20 @@ const Navibar2 = () => {
     <div>
       <div className={Styles.navibar2_component}>
         <div className={Styles.navibar_align}>
-          <Link to={"/home"}><img className={Styles.navibar2_logo} src="./nav-logo.svg" alt="logo" /></Link>
+          <NavLink exact to={"/home"}><img className={Styles.navibar2_logo} src="./nav-logo.svg" alt="logo" /></NavLink>
         </div>
         <div className={Styles.navibar2_list}>
-          <Link className={Styles.link} to={"/createandplay"}>Create Quiz</Link>
-          <Link className={Styles.link} to={"/library"}>Library</Link>
-          <Link className={Styles.link} to={"/maintaince"}>Discover</Link>
-          <Link className={Styles.link} to={"/maintaince"}>Notifications</Link>
-          <Link className={Styles.link} to={"/profile"}>Profile</Link>
-          <Link className={Styles.link} to={"/leaderboard"}>Leaderbord</Link>
-          <Link className={Styles.link} to={"/help"}>Help</Link>
-          <Link className={Styles.link} to={"/contact"}>Contact Us</Link>
+          <ul className={Styles.navi_bar_link_list}>
+            <li className={Styles.link}><NavLink to={"/home"} className={Styles.link_list}>Home</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/createandplay"} className={Styles.link_list}>Create Quiz</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/library"} className={Styles.link_list}>Library</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/maintaince"} className={Styles.link_list}>Discover</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/leaderboard"} className={Styles.link_list}>Leaderbord</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/maintaince"} className={Styles.link_list}>Notifications</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/profile"} className={Styles.link_list}>Profile</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/help"} className={Styles.link_list}>Help</NavLink></li>
+            <li className={Styles.link}><NavLink to={"/contact"} className={Styles.link_list}>Contact Us</NavLink></li>
+          </ul>
         </div>
       </div>
     </div>
